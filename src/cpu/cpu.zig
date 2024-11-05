848d41e2e6f6ef45e9426647eef3dfc5ab42bbfc
OsakiTsukiko <osakitsukiko.mail@gmail.com>
2024-11-05 23:10:16 +0200

    update cpu

--- src/cpu/cpu.zig
+++ src/cpu/cpu.zig
@@ -3,33 +3,27 @@ const Registers = @import("./registers.zig").Registers;
 const Interpretor = @import("./interpretor.zig").Interpretor;
 
 pub const CPU = struct {
-    registers: Registers,
+    registers: Registers = Registers{
+        .r8 = .{ .l = .{
+            .b = 0x00,
+            .c = 0x00,
+            .d = 0x00,
+            .e = 0x00,
+            .h = 0x00,
+            .l = 0x00,
+            .flags = .{
+                .zero = false,
+                .substract = false,
+                .half_carry = 0,
+                .carry = 0,
+            },
+            .a = 0x00,
+            .sp = 0x0000,
+            .pc = 0x0000,
+        } }
+    },
     bus: MemoryBus = MemoryBus{},
-
-    pub fn init() CPU {
-        return CPU {
-            .registers = Registers{
-                .r8 = .{ .l = .{
-                    .b = 0x00,
-                    .c = 0x00,
-                    .d = 0x00,
-                    .e = 0x00,
-                    .h = 0x00,
-                    .l = 0x00,
-                    .flags = .{
-                        .zero = false,
-                        .substract = false,
-                        .half_carry = 0,
-                        .carry = 0,
-                    },
-                    .a = 0x00,
-                    .sp = 0x0000,
-                    .pc = 0x0000,
-                } }
-            }
-        };
-    }
-
+    
     pub const MemoryBus = struct {
         memory: [0xFFFF]u8 = std.mem.zeroes([0xFFFF]u8),
 
